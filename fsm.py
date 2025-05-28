570d40b4cbb12a279d54002798beb9b0cbdc1d19
Reginakh0804 <reginakh0804@gmail.com>
2025-05-28 12:17:12 +0500

    Delete fsm.py

--- fsm.py
+++
@@ -1,9 +0,0 @@
-
-from aiogram.dispatcher.filters.state import State, StatesGroup
-
-class Register(StatesGroup):
-    Language = State()
-    Phone = State()
-    Name = State()
-    Group = State()
-    Message = State()
